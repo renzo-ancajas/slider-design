f1043097de923dfe687380129ef6c15c617271a8
renzoancajas24 <108566575+renzo-ancajas@users.noreply.github.com>
2022-08-02 14:55:58 +0800

    Testing image path

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
 import react from React
 
-import animalpics from "./Icons";
+import animalpics from "./Icons"; // path of images
 import '../Styles/App.css';
+import { render } from "@testing-library/react";
 
 function App() {
-
-    return (
-        
-        <>
-
-        </>
-
-    )
-
-}
+    render()
+        return (
+            
+            <img src={animalpics} alt={"animalRacoon"}/>
+        )
+    
+};
 
 export default App;
--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -1,3 +1,17 @@
-import React from "react";
+import React, {useState} from 'react';
+import dataSlider from "../Components";
+import '../Styles/Slider.css';
+
+
+export default function Slider() {
+    return (
+
+        <div className="container-slide">
+            {}
+        </div>
+
+    )
+
+}
 
 
