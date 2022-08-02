6b6a31069bfe4e0ce1b50c60568478a1598c5f00
renzoancajas24 <108566575+renzo-ancajas@users.noreply.github.com>
2022-08-02 14:41:18 +0800

    Added Components, Icons, Styles, & src

---
+++ src/Components/dataSlider.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const dataSlider = [
+
+    {
+        id: racoon1(),
+        title: "Racoon Snippet",
+        subtitle: "by John Smith"
+    },
+
+    {
+        id: racoon2(),
+        title: "Racoon City",
+        subtitle: "by Leon Kennedy"
+    },
+
+    {
+        id: capybara(),
+        title: "He pulled up, hop out the afterparty.",
+        subtitle: "Sippin' on that Henn'"
+    }
+
+]
